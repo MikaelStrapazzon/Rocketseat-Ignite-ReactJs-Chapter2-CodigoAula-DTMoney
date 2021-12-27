318e851b474f86625efbe6d581bfd49550beaf6b
MikaelStrapazzon <mikael.quadros@gmail.com>
2021-12-27 17:56:34 -0300

    Desenvolvendo modal de adicionar transações

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,35 @@
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website"
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          caregory: "Food",
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all( "transaction" )
+    })
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse( request.requestBody )
+
+      return schema.create( "transaction", data );
     })
   }
 });
