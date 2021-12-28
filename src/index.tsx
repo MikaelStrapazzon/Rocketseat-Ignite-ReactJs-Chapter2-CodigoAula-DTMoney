0b574f9cfe933958486806fe201d8e59e71a367b
MikaelStrapazzon <mikael.quadros@gmail.com>
2021-12-28 04:45:56 -0300

    Desenvolvimento

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,19 @@ createServer({
       transactions: [
         {
           id: 1,
-          title: "Freelance de website"
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-14 11:00:00")
         }
       ]
     })
