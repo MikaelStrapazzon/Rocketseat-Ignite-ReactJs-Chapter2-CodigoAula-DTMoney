06b4b88a3cca0d8305f25e2318930671eb49457f
MikaelStrapazzon <mikael.quadros@gmail.com>
2021-12-28 18:09:34 -0300

    Finalização

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,6 @@
-import { FormEvent, useContext, useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
-
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import { Container, TransactionTypeContainer, RadioBox } from "./style";
 
@@ -15,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
@@ -35,7 +34,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     setTitle("");
     setAmount(0);
     setCategory("");
-    setCategory("deposit");
+    setType("deposit");
 
     onRequestClose();
   };
